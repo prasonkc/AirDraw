c9c9c24fc602123998042b0464d48fa9dddb969a
Prason Kc <prasonkc07@gmail.com>
2025-10-07 23:39:31 +0545

    Setup hands and face detection

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import mediapipe as mp
 
 # Initialize hands module
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
+hands = mp_hands.Hands(max_num_hands=2)
+
+# Initialize face module
+mp_face = mp.solutions.face_detection
+face_detection = mp_face.FaceDetection(min_detection_confidence=0.8)
 
 # Initialize MediaPipe drawing module
 mp_drawing = mp.solutions.drawing_utils
@@ -21,17 +25,25 @@ while cam.isOpened():
     # Convert the frame to RGB format
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Process the frame to detect hands
-    results = hands.process(frame_rgb)
+    # Process the frame to detect hands and face
+    hand_results = hands.process(frame_rgb)
+    face_results = face_detection.process(frame_rgb)
 
+    # --------------------HANDS----------------------------------------
     # Check if hands are detected
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
+    if hand_results.multi_hand_landmarks:
+        for hand_landmarks in hand_results.multi_hand_landmarks:
             # Draw landmarks on the frame
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-    # Display the frame with hand landmarks
-    cv2.imshow('Hand Recognition', frame)
+    # --------------------FACE--------------------------------------------
+    # Check if face are detected
+    if face_results.detections:
+        for face in face_results.detections:
+            mp_drawing.draw_detection(frame, face)
+
+    # Display the frame with hand + face landmarks
+    cv2.imshow('Hand + Face Recognizations', frame)
 
     # Exit when 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
